35113f01567813b108ade2900b4a653ea67bc980
James Deville <james@james-devilles-macbook-pro.local>
2008-10-20 13:01:40 -0700

    modified default route to hellos_controller, made hellos/index show the individual hellos

--- config/router.rb
+++ config/router.rb
@@ -32,6 +32,7 @@ Merb::Router.prepare do
   
   # Adds the required routes for merb-auth using the password slice
   slice(:merb_auth_slice_password, :name_prefix => nil, :path_prefix => "")
+  
 
   # This is the default route for /:controller/:action/:id
   # This is fine for most cases.  If you're heavily using resource-based
@@ -40,5 +41,5 @@ Merb::Router.prepare do
   default_routes
   
   # Change this for your home page to be available at /
-  # match('/').to(:controller => 'whatever', :action =>'index')
+  match('/').to(:controller => 'hellos', :action =>'index')
 end
