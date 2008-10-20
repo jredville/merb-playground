36a49ef76ad8d30bbc9f051148f75767c1bccd7f
James Deville <james@james-devilles-macbook-pro.local>
2008-10-20 12:56:59 -0700

    added methods to hello model

--- app/models/hello.rb
+++ app/models/hello.rb
@@ -2,6 +2,9 @@ class Hello
   include DataMapper::Resource
   
   property :id, Serial
+  property :name, String
 
-
+  def say_hello
+	  "Hello, my id is #{id}. My name is #{name}. I'm here to say 'Hello!'"
+	end
 end
